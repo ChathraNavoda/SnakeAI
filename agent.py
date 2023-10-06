cfd5ecab296164666a6d356c56c7761bb88dd074
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-10-06 17:35:57 +0530

    training loop for the AI agent
    Implemented the training loop for the Snake game AI agent.

--- agent.py
+++ agent.py
@@ -75,3 +75,49 @@ class Agent:
             final_move[move] = 1
 
         return final_move
+    
+    def train():
+        plot_scores = []
+        plot_mean_scores = []
+        total_score = 0
+        record = 0
+        agent = Agent()
+        game = SnakeGameAI()
+        
+        while True:
+            # Get old state
+            state_old = agent.get_state(game)
+
+            # Get move
+            final_move = agent.get_action(state_old)
+
+            # Perform move and get new state
+            reward, done, score = game.play_step(final_move)
+            state_new = agent.get_state(game)
+
+            # Train short memory
+            agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+            # Remember
+            agent.remember(state_old, final_move, reward, state_new, done)
+
+            if done:
+                # Train the long memory, plot result
+                game.reset()
+                agent.n_games += 1
+                agent.train_long_memory()
+
+                if score > record:
+                    record = score
+                    agent.model.save()
+
+                print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+                plot_scores.append(score)
+                total_score += score
+                mean_score = total_score / agent.n_games
+                plot_mean_scores.append(mean_score)
+                plot(plot_scores, plot_mean_scores)
+
+    if __name__ == '__main__':
+        train()
