89130dc869ff2850c92e49221cecdbe2c40c1bb6
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-10-06 17:21:27 +0530

    Add PPO Training Plotting Function

---
+++ helper.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from IPython import display
+
+plt.ion()
+
+def plot_ppo(scores, mean_scores):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    plt.title('Training...')
